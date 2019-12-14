6e240153e916e9c4a0a14b61f31aca9cb81d9241
unknown <Admin@IHR80PBF231.continental.edu.pe>
2019-12-14 12:57:45 -0500

    cambios de inicio

---
+++ app/src/main/java/com/example/limonp_repositorio/FragmentPersonal.java
@@ -0,0 +1,21 @@
+package com.example.limonp_repositorio;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentPersonal extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment_personal, container, false);
+    }
+}
+
