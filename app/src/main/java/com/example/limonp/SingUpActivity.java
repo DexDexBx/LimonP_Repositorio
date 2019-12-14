653b8b09e9d60ea618da6d3d8fe35e327c3bd0cf
unknown <Admin@IHR80PBF431.continental.edu.pe>
2019-12-14 05:46:09 -0500

    Parte 2

--- app/src/main/java/com/example/limonp/SingUpActivity.java
+++ app/src/main/java/com/example/limonp/SingUpActivity.java
@@ -6,9 +6,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class SingUpActivity extends AppCompatActivity {
 
     RelativeLayout rellay1,rellay2;
 
@@ -20,11 +19,10 @@ public class LoginActivity extends AppCompatActivity {
             rellay2.setVisibility(View.VISIBLE);
         }
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sing_up);
 
         rellay1=(RelativeLayout) findViewById(R.id.rellay1);
         rellay2=(RelativeLayout) findViewById(R.id.rellay2);
